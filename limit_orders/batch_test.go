e7fc4a49be3566a9a9a9f35d100d09905bbb261a
0xKitsune <0xKitsune@protonmail.com>
2022-09-29 20:47:36 -0400

    adding tests for batching

--- limit_orders/batch_test.go
+++ limit_orders/batch_test.go
@@ -1,1 +1,15 @@
 package limitOrders
+
+import (
+	"testing"
+)
+
+func TestQuickSortOrderGroup(t *testing.T) {
+
+}
+
+func TestOrderGroupsByValue(t *testing.T) {
+
+}
+
+func TestSimulateAndBatchOrders(t *testing.T) {}
