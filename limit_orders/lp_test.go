5c51a4d5892dea06005437a210cb5f6849750b43
0xKitsune <0xKitsune@protonmail.com>
2022-10-04 15:10:15 -0400

    added tests for lp package

--- limit_orders/lp_test.go
+++ limit_orders/lp_test.go
@@ -1,1 +1,50 @@
 package limitOrders
+
+import "testing"
+
+func TestInitializeLPReserves(t *testing.T) {
+	// TODO:
+}
+
+func TestGetLPReserves(t *testing.T) {
+	// TODO:
+}
+
+func TestGetUniV2LPReserves(t *testing.T) {
+	// TODO:
+}
+
+func TestGetUniV3LPReserves(t *testing.T) {
+	// TODO:
+}
+
+func TestSetReserves(t *testing.T) {
+	// TODO:
+}
+
+func TestSetReservesAndUpdatePriceOfTokenPerWeth(t *testing.T) {
+	// TODO:
+}
+
+func TestUpdatePriceOfTokenPerWeth(t *testing.T) {
+	// TODO:
+}
+
+func TestConvertAmountsToCommonDecmials(t *testing.T) {
+	// TODO:
+
+}
+func TestGetLPToken0(t *testing.T) {
+	// TODO:
+
+}
+
+func TestGetLPToken1(t *testing.T) {
+	// TODO:
+
+}
+
+func TestGetTokenDecimals(t *testing.T) {
+	// TODO:
+
+}
