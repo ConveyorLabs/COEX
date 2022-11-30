c5b2e379448a723bd9becd88b547b5e82d272cd1
0xKitsune <0xKitsune@protonmail.com>
2022-11-30 11:19:52 -0500

    updated simulation logic, bumped to pair sync v 0.4.2

--- src/orders/simulate.rs
+++ src/orders/simulate.rs
@@ -10,17 +10,20 @@ use ethers::{
 };
 use pair_sync::pool::Pool;
 
-use crate::markets::market::{self, get_market_id};
+use crate::{
+    error::BeltError,
+    markets::market::{self, get_market_id},
+};
 
 use super::sandbox_limit_order::SandboxLimitOrder;
 
 //Takes a hashmap of market to sandbox limit orders that are ready to execute
-pub fn simulate_and_batch_sandbox_limit_orders<P: 'static + JsonRpcClient>(
+pub async fn simulate_and_batch_sandbox_limit_orders<P: 'static + JsonRpcClient>(
     sandbox_limit_orders: HashMap<H256, &SandboxLimitOrder>,
     mut simulated_markets: HashMap<u64, HashMap<H160, Pool>>,
     v3_quoter_address: H160,
     provider: Arc<Provider<P>>,
-) {
+) -> Result<(), BeltError<P>> {
     //Go through the slice of sandbox limit orders and group the orders by market
     let mut orders_grouped_by_market: HashMap<u64, Vec<&SandboxLimitOrder>> = HashMap::new();
 
@@ -45,6 +48,7 @@ pub fn simulate_and_batch_sandbox_limit_orders<P: 'static + JsonRpcClient>(
 
                 for (pool_address, pool) in simulated_market {
                     if pool.calculate_price(order.token_in) >= order.price {
+                        //simulate the swap and get the amount out
                         let amount_out = match pool {
                             Pool::UniswapV2(uniswap_v2_pool) => uniswap_v2_pool
                                 .simulate_swap(order.token_in, order.amount_in_remaining),
@@ -71,6 +75,8 @@ pub fn simulate_and_batch_sandbox_limit_orders<P: 'static + JsonRpcClient>(
     }
 
     //When the market is tapped out for the orders, move onto the next market
+
+    Ok(())
 }
 
 fn sort_sandbox_limit_orders_by_amount_in(
